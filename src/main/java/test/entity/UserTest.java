24bb6e1e26a5b993b8d2e172d9b53344912d48f4
geeklovetg <geekloveloli@gmail.com>
2019-07-23 09:25:48 +0800

    v1.3 修改部分页面

---
+++ src/main/java/test/entity/UserTest.java
@@ -0,0 +1,125 @@
+package test.entity; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* User Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÆßÔÂ 22, 2019</pre> 
+* @version 1.0 
+*/ 
+public class UserTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getPhoneNumber() 
+* 
+*/ 
+@Test
+public void testGetPhoneNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPhoneNumber(String phoneNumber) 
+* 
+*/ 
+@Test
+public void testSetPhoneNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getEmail() 
+* 
+*/ 
+@Test
+public void testGetEmail() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setEmail(String email) 
+* 
+*/ 
+@Test
+public void testSetEmail() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUid() 
+* 
+*/ 
+@Test
+public void testGetUid() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setUid(int id) 
+* 
+*/ 
+@Test
+public void testSetUid() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUsername() 
+* 
+*/ 
+@Test
+public void testGetUsername() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setUsername(String username) 
+* 
+*/ 
+@Test
+public void testSetUsername() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPassword() 
+* 
+*/ 
+@Test
+public void testGetPassword() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPassword(String password) 
+* 
+*/ 
+@Test
+public void testSetPassword() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/main/java/test/service/PublishServiceTest.java
@@ -0,0 +1,40 @@
+package test.service; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/** 
+* PublishService Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 22, 2019</pre> 
+* @version 1.0 
+*/ 
+public class PublishServiceTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: publishArtical(HttpServletRequest request) 
+* 
+*/ 
+@Test
+public void testPublishArtical() throws Exception { 
+//TODO: Test goes here...
+
+} 
+
+
+} 
